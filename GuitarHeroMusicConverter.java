f427b0f6f035115dc757139ab5c79eb64f016174
Jake <jake@jakekarasik.com>
2017-01-09 19:56:29 -0500

    undo test

--- GuitarHeroMusicConverter.java
+++ GuitarHeroMusicConverter.java
@@ -4,7 +4,7 @@ public class GuitarHeroMusicConverter {
 
     
     public static void main(String[] args) {
-       System.out.println("pull test");
+       
        Data data = new Data();
        String response = data.uploadSong("https://jakekarasik.com/Idols.ogg");
        Notes[] notes = data.downloadSongData(response);
